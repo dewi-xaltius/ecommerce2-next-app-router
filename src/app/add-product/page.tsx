8a49df8c5e2acb424fea140c06f6ebbbcd5d9c5a
dewi-xaltius <smdewi@xaltius.tech>
2024-08-28 08:08:18 +0800

    add AddProductForm component

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,9 @@
+import AddProductForm from '../components/AddProductForm';
+
+export default function AddProductPage() {
+  return (
+    <main className="flex justify-center items-center h-screen bg-gray-100">
+      <AddProductForm />
+    </main>
+  );
+}
